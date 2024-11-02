55d7453d0d8c751e6fd47912c2f5a4c26d18fe4f
Janice Shih <janiceshih@Janices-MacBook-Pro.local>
2024-11-02 19:07:00 -0400

    Install sass-loader

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  sassOptions: {
+    // You can pass options to the sass render function here
+    // For example, to silence deprecation warnings:
+    silenceDeprecations: ['legacy-js-api'],
+  },
 };
 
 export default nextConfig;
