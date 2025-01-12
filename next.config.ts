6b5aabe88e4b7e3361df8e5793197b7c54016c10
Janice Shih <janiceshih@Janices-MacBook-Pro.local>
2025-01-12 16:13:48 -0500

    Add output: 'standalone'

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,7 @@ const nextConfig: NextConfig = {
     // For example, to silence deprecation warnings:
     silenceDeprecations: ['legacy-js-api'],
   },
+  output: 'standalone',
 };
 
 export default nextConfig;
